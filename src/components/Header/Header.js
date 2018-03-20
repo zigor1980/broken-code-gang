60d203920b464908e30fe85c35fe6caf5dfcdba7
Kirill Cherkalov <k.cherkalov@yandex.com>
2018-03-20 11:24:53 +0300

    new folder for component HeaderCenterItems

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './Header.css';
-import { HeaderCenterItems } from './HeaderCenterItems.js';
+import { HeaderCenterItems } from '../HeaderCenterItems/HeaderCenterItems';
 
 export function  Header(props) {
     return (
-        <header className={'Header'}>
+        <header className='Header'>
             <button>{props.buttonExit && 'Назад' }</button>
             <HeaderCenterItems groupName='Telegram' participants='0 участников' /> 
             <button>{props.buttonHeaderRight && 'Свойства/поиск' }</button>
---
+++ src/components/HeaderCenterItems/HeaderCenterItems.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export function HeaderCenterItems (props) {
+    return (
+        <div className='Header__content'>
+            <img src='http://localhost:3000/static/media/logo.5d5d9eef.svg' style = {{width: '85px'}} />
+             <div className='Header__content__item'>
+                <h2 >{props.groupName}</h2>
+                <p>{props.participants}</p>
+            </div>
+        </div>
+    );
+}

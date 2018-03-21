204dac87c87a58638cad7ce64c087a5dbd9016d5
Kirill Cherkalov <k.cherkalov@yandex.com>
2018-03-21 17:00:33 +0300

    fixed the code on Arthur comments

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './Header.css';
 import { HeaderCenterItems } from '../HeaderCenterItems/HeaderCenterItems';
 
--- src/components/HeaderCenterItems/HeaderCenterItems.js
+++ src/components/HeaderCenterItems/HeaderCenterItems.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 export function HeaderCenterItems (props) {
     return (

4a4af5a66455d6ddbe6b6d02c07153e179639da0
Artur Minchukov <arturminchukov@gmail.com>
2018-03-24 19:28:57 +0300

    Header: styles changed and component added to storybook

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './Header.css';
 import { HeaderCenterItems } from '../HeaderCenterItems/HeaderCenterItems';
 
-export function  Header(props) {
+export function Header(props) {
     return (
         <header className='Header'>
-            <button>{props.buttonExit && 'Назад' }</button>
-            <HeaderCenterItems groupName='Telegram' participants='0 участников' /> 
-            <button>{props.buttonHeaderRight && 'Свойства/поиск' }</button>
+            <button className="Header__button">{props.buttonExit && 'Назад' }</button>
+            <HeaderCenterItems groupName='Telegram' participants='0 участников'/>
+            <button className="Header__button">{props.buttonHeaderRight && 'Свойства/поиск' }</button>
         </header>
     );
 }
--- src/storybook/config.js
+++ src/storybook/config.js
@@ -4,6 +4,7 @@ function loadStories() {
     require('./Avatar.js');
     require('./ChatField.js');
     require('./InstanceSummaryElement.js');
+    require('./Header.js');
 }
 
 configure(loadStories, module);

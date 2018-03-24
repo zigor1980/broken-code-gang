8c52c56faa27c513f4e14a9ce41f97f1c390733e
Artur Minchukov <arturminchukov@gmail.com>
2018-03-24 19:51:55 +0300

    ChatPage: component created and added to storybook

--- src/storybook/config.js
+++ src/storybook/config.js
@@ -3,6 +3,7 @@ import {configure} from '@storybook/react';
 function loadStories() {
     require('./Avatar.js');
     require('./ChatField.js');
+    require('./ChatPage.js');
 }
 
 configure(loadStories, module);

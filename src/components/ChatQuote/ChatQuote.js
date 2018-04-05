db592769b0ba1a9689d938f1c515fe87ab186189
bibushik <bibushik@yandex.ru>
2018-04-06 00:10:27 +0300

    chat list styles part1

--- src/components/ChatQuote/ChatQuote.js
+++ src/components/ChatQuote/ChatQuote.js
@@ -4,22 +4,19 @@ import './ChatQuote.css';
 export function ChatQuote(props) {
     const { message: text, created_at: date, userId: authorId } = props.message,
         userId = props.userId;
-    let angleDirection = 'ChatQuote__angle_right',
-        chatDirection = 'ChatQuote_right',
+    let chatDirection = 'ChatQuote_right',
         user = '';
 
     if (userId !== authorId) {
-        angleDirection = 'ChatQuote__angle_left';
         chatDirection = 'ChatQuote_left';
         user = <p className="ChatQuote__user">{authorId}</p>;
     }
 
     return (
-      <div className={`ChatQuote ${chatDirection}`}>
-            <div className={angleDirection} />
-          {user}
-          <p className="ChatQuote__text">{text}</p>
-          <p className="ChatQuote__timestamp">{date}</p>
+        <div className={`ChatQuote ${chatDirection}`}>
+            {user}
+            <p className="ChatQuote__text">{text}</p>
+            <p className="ChatQuote__timestamp">{date}</p>
         </div>
     );
 }

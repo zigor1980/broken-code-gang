60567c6662acc39b5fc086bcbc077c5077e28779
bibushik <bibushik@yandex.ru>
2018-04-07 23:24:21 +0300

    add navigation from chatlist to chatpage

--- src/components/ChatList/ChatList.js
+++ src/components/ChatList/ChatList.js
@@ -2,23 +2,42 @@ import * as React from 'react';
 import { InstanceSummaryElement } from '../InstanceSummaryElement/InstanceSummaryElement';
 import './ChatList.css';
 import { InfiniteRooms } from '../InfiniteRooms/InfiniteRooms';
+import { connect } from 'react-redux';
+import { routeNavigation } from '../../actions/route';
 
-function ChatList({ rooms, fetchNext }) {
-    return (
-        <InfiniteRooms fetchNext={fetchNext}>
-            {rooms.map(room => (
-                <InstanceSummaryElement
-                    key={room._id}
-                    summary={{
-                        avatar: {
-                            src: 'https://avatars.mds.yandex.net/get-pdb/1008348/cab77028-8042-4d20-b343-a1498455e4c8/s1200',
-                            modifier: 'avatar-s',
-                        },
-                        title: `${room.name}`,
-                    }}
-                />))}
-        </InfiniteRooms>
-    );
-}
+const stateToProps = state => ({
+    payload: state.route.payload,
+});
 
-export default ChatList;
+export const ChatList = connect(stateToProps)(class ChatList extends React.Component {
+    enterRoom(roomId) {
+        this.props.dispatch(routeNavigation({
+            page: 'chat_page',
+            payload: {
+                ...this.props.payload,
+                currentRoom: roomId,
+            },
+        }));
+    }
+    render() {
+        const { rooms, fetchNext } = this.props;
+        return (
+            <InfiniteRooms fetchNext={fetchNext}>
+                {rooms.map(room => (
+                    <InstanceSummaryElement
+                        key={room._id}
+                        summary={{
+                            avatar: {
+                                src: 'https://avatars.mds.yandex.net/get-pdb/1008348/cab77028-8042-4d20-b343-a1498455e4c8/s1200',
+                                modifier: 'avatar-s',
+                            },
+                            title: `${room.name}`,
+                            id: `${room._id}`,
+                        }}
+                        onclick={this.enterRoom.bind(this)}
+                    />))}
+            </InfiniteRooms>
+        );
+    }
+});
+
--- src/components/InstanceSummaryElement/InstanceSummaryElement.js
+++ src/components/InstanceSummaryElement/InstanceSummaryElement.js
@@ -1,46 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Avatar } from '../Avatar/Avatar';
 import './InstanceSummaryElement.css';
 
-export function InstanceSummaryElement(props) {
-    if (!props) {
-        return null;
+export class InstanceSummaryElement extends Component {
+
+    handleClick = () => {
+        this.props.onclick(this.props.summary.id);
     }
 
-    let { onclick, summary } = props;
-    const { avatar, title, description, author, descModifiers } = summary;
+    render() {
+        if (!this.props) {
+            return null;
+        }
 
-    let titleClasses = 'InstanceSummaryElement__title';
-    let descClasses = 'InstanceSummaryElement__desc';
+        const { summary } = this.props;
+        const {
+            avatar, title, description, author, descModifiers,
+        } = summary;
 
-    if (descModifiers && descModifiers === 'light') {
-        titleClasses += ' InstanceSummaryElement__title_light';
-        descClasses += ' InstanceSummaryElement__desc_light';
-    } else {
-        descClasses += ' InstanceSummaryElement__desc_dark';
-    }
+        let titleClasses = 'InstanceSummaryElement__title';
+        let descClasses = 'InstanceSummaryElement__desc';
 
-    let descView = '';
-    if (author) {
-        descView = (<p className={descClasses}>
-          <span className="InstanceSummaryElement__author">{`${author}: `}</span>
-          {description}
-                    </p>);
-    } else {
-        descView = (<p className={descClasses}>
-          {description}
-                    </p>);
-    }
+        if (descModifiers && descModifiers === 'light') {
+            titleClasses += ' InstanceSummaryElement__title_light';
+            descClasses += ' InstanceSummaryElement__desc_light';
+        } else {
+            descClasses += ' InstanceSummaryElement__desc_dark';
+        }
 
-    return (
-      <div className="InstanceSummaryElement" onClick={onclick}>
-          <div className="InstanceSummaryElement__avatar">
-              <Avatar image={avatar} />
+        let descView = '';
+        if (author) {
+            descView = (<p className={descClasses}>
+                <span className="InstanceSummaryElement__author">{`${author}: `}</span>
+                {description}
+            </p>);
+        } else {
+            descView = (<p className={descClasses}>
+                {description}
+            </p>);
+        }
+
+        return (
+            <div className="InstanceSummaryElement" onClick={this.handleClick}>
+                <div className="InstanceSummaryElement__avatar">
+                    <Avatar image={avatar} />
+                </div>
+                <div className="InstanceSummaryElement__info">
+                    <h3 className={titleClasses}>{title}</h3>
+                    {descView}
+                </div>
             </div>
-          <div className="InstanceSummaryElement__info">
-              <h3 className={titleClasses}>{title}</h3>
-              {descView}
-            </div>
-        </div>
-    );
+        );
+    }
 }
+

1f416df6293af1f3ecaa0183eea5436162d0aae2
Maxim Kalinovskiy <malekylik@gmail.com>
2018-04-10 22:13:31 +0300

    added a basic validation and a basic registration

--- src/components/AuthorizationPage/AuthorizationPage.js
+++ src/components/AuthorizationPage/AuthorizationPage.js
@@ -3,6 +3,28 @@ import './AuthorizationPage.css';
 import {connect} from 'react-redux'
 
 import {routeNavigation} from '../../actions/route';
+import api from '../../api';
+
+const updateInputField = (inputsState, input, field, value) => {
+    return {
+        ...inputsState,
+        [input]: {
+            ...inputsState[input],
+            [field]: value
+        }
+    }
+};
+
+const inputsInitial = {
+    login: {
+        value: '',
+        error: false
+    },
+    password: {
+        value: '',
+        error: false
+    },
+};
 
 export const AuthorizationPage = connect()(
     class AuthorizationPage extends React.Component {
@@ -10,9 +32,11 @@ export const AuthorizationPage = connect()(
             super(props);
             this.state = {
                 active: 'Sign in',
+                inputs: inputsInitial
             };
             this.onclick = this.onclick.bind(this);
             this.submitHandler = this.submitHandler.bind(this);
+            this.fieldChangedHandler = this.fieldChangedHandler.bind(this);
         }
 
         onclick(button) {
@@ -22,6 +46,57 @@ export const AuthorizationPage = connect()(
         }
 
         submitHandler() {
+            const { login: { value: login }, password: { value: password} } = this.state.inputs;
+
+            let inputs = this.state.inputs;
+
+            if (login === '') {
+                inputs = updateInputField(inputs, 'login', 'error', true);
+            }
+
+            if (password === '') {
+                inputs = updateInputField(inputs, 'password', 'error', true);
+            }
+
+            if (login === '' || password === '') {
+                this.setState({inputs});
+                return;
+            }
+
+            if (this.state.active === 'Sign up') {
+                console.log('sing up');
+                this.singUp(login, password);
+            }
+
+            if (this.state.active === 'Sign in') {
+                console.log('sing in');
+                this.singIn(login, password);
+            }
+        }
+
+        fieldChangedHandler(e) {
+            this.setState({
+                    inputs: updateInputField(this.state.inputs, e.target.name, 'value', e.target.value)
+                })
+        }
+
+        async singUp(login, password) {
+            const user = await api.getUserByLogin(login, password);
+            console.log(user);
+
+            if (!user) {
+                await api.addUser(login, password);
+            }
+
+            this.setState({
+                active: 'Sign in',
+                inputs: inputsInitial
+            });
+        }
+
+        async singIn(login, password) {
+
+
             this.props.dispatch(routeNavigation({
                 page: 'chat_list',
                 payload: {
@@ -34,11 +109,13 @@ export const AuthorizationPage = connect()(
 
         render() {
             const swapButton = 'Sign up' === this.state.active ? 'Sign in' : 'Sign up';
+
+            const { login: { value: login }, password: { value: password }, login: { error: loginError }, password: { error: passwordError } } = this.state.inputs;
             return (
                 <div className="AuthorizationPage">
                     <img className="AuthorizationPage__Image" src={require('../../assets/icons/logo.png')} alt="logo"/>
-                    <input className="AuthorizationPage__Input" type="text" name="login" placeholder="Login" required/>
-                    <input className="AuthorizationPage__Input" type="password" name="password" placeholder="Password"
+                    <input value={login} className="AuthorizationPage__Input" style={{animationName: loginError ? 'input-error' : ''}} onChange={this.fieldChangedHandler} onAnimationEnd={() => {this.setState({inputs: updateInputField(this.state.inputs, 'login', 'error', false)})}} type="email" name="login" placeholder="Email" required/>
+                    <input value={password} className="AuthorizationPage__Input" style={{animationName: passwordError ? 'input-error' : ''}} onChange={this.fieldChangedHandler} onAnimationEnd={() => {this.setState({inputs: updateInputField(this.state.inputs, 'password', 'error', false)})}} type="password" name="password" placeholder="Password"
                            required/>
                     <button className="AuthorizationPage__submit"
                             onClick={this.submitHandler}>{this.state.active}</button>

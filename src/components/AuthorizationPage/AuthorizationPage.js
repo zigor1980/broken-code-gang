779c14a1b45c7dad31ee49e18951a1f2af6c05ca
Maxim Kalinovskiy <malekylik@gmail.com>
2018-04-10 23:50:05 +0300

    added the authorization message

--- src/components/AuthorizationPage/AuthorizationPage.js
+++ src/components/AuthorizationPage/AuthorizationPage.js
@@ -5,6 +5,7 @@ import {connect} from 'react-redux'
 import {routeNavigation} from '../../actions/route';
 import api from '../../api';
 import signInUser from '../../actions/signInUser';
+import Message from '../Message/Message';
 
 const updateInputField = (inputsState, input, field, value) => {
     return {
@@ -33,7 +34,8 @@ export const AuthorizationPage = connect()(
             super(props);
             this.state = {
                 active: 'Sign in',
-                inputs: inputsInitial
+                inputs: inputsInitial,
+                message: ''
             };
             this.onclick = this.onclick.bind(this);
             this.submitHandler = this.submitHandler.bind(this);
@@ -43,6 +45,7 @@ export const AuthorizationPage = connect()(
         onclick(button) {
             this.setState({
                 active: button.target.name,
+                message: ''
             });
         }
 
@@ -75,7 +78,8 @@ export const AuthorizationPage = connect()(
 
         fieldChangedHandler(e) {
             this.setState({
-                    inputs: updateInputField(this.state.inputs, e.target.name, 'value', e.target.value)
+                    inputs: updateInputField(this.state.inputs, e.target.name, 'value', e.target.value),
+                    message: ''
                 })
         }
 
@@ -83,10 +87,13 @@ export const AuthorizationPage = connect()(
             try{
                 const user = await api.getUserByLogin(login, password);
                 console.log(user);
-    
-                if (!user) {
-                    await api.addUser(login, password);
+
+                if (user) {
+                    this.setState({message: 'Такой логин уже занят.'});
+                    return;
                 }
+    
+                await api.addUser(login, password);
     
                 this.setState({
                     active: 'Sign in',
@@ -99,6 +106,12 @@ export const AuthorizationPage = connect()(
 
         async singIn(login, password) {
             const user = await this.props.dispatch(signInUser(login, password));
+
+            if (!user) {
+                this.setState({message: 'Пользователя с таким логином и паролем не найдено.'});
+                return;
+            }
+
             console.log('user: ', user);
 
             if (user) {
@@ -119,6 +132,9 @@ export const AuthorizationPage = connect()(
             const { login: { value: login }, password: { value: password }, login: { error: loginError }, password: { error: passwordError } } = this.state.inputs;
             return (
                 <div className="AuthorizationPage">
+
+                    {this.state.message && <Message text={this.state.message} />}
+
                     <img className="AuthorizationPage__Image" src={require('../../assets/icons/logo.png')} alt="logo"/>
                     <input value={login} className="AuthorizationPage__Input" style={{animationName: loginError ? 'input-error' : ''}} onChange={this.fieldChangedHandler} onAnimationEnd={() => {this.setState({inputs: updateInputField(this.state.inputs, 'login', 'error', false)})}} type="email" name="login" placeholder="Email" required/>
                     <input value={password} className="AuthorizationPage__Input" style={{animationName: passwordError ? 'input-error' : ''}} onChange={this.fieldChangedHandler} onAnimationEnd={() => {this.setState({inputs: updateInputField(this.state.inputs, 'password', 'error', false)})}} type="password" name="password" placeholder="Password"

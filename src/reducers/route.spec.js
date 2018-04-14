9dc75c5cecfaaf272d87712f32bcfffa4f9ee737
Maxim Kalinovskiy <malekylik@gmail.com>
2018-04-14 18:08:06 +0300

    added tests for route reducer

---
+++ src/reducers/route.spec.js
@@ -0,0 +1,52 @@
+import { expect } from 'chai';
+import route from './route';
+import { routeNavigation } from '../actions/route'
+
+describe('Reducer::Route', function() {
+    it('returns route (object), page ("authorization"), payload (empty object) if state is undefined', function() {
+        // setup
+        const state = null;
+        const expectedNewState = {
+            page: 'authorization',
+            payload: {}
+        };
+
+        // execute
+        const newState = route(state, { type: 'unknown' });
+
+        // verify
+        expect(newState).to.deep.equal(expectedNewState);
+    });
+
+    it('on ROUTE_NAVIGATE returns new route with set page and payload', function() {
+    // setup
+    const state = {
+        page: 'authorization',
+        payload: {
+            FooterNav: 'active'
+        }
+    };
+
+    const routeMessage = { 
+        page: 'chat_list',
+        payload: {
+            _id: '123asd'
+        }
+    };
+
+    const action = routeNavigation(routeMessage);
+
+    const expectedNewState = { 
+        page: 'chat_list',
+        payload: {
+            _id: '123asd'
+        }
+    };
+    // execute
+
+    const newState = route(state, action);
+
+    // verify
+    expect(newState).to.deep.equal(expectedNewState);
+    });
+});

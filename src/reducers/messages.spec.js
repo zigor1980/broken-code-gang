98ce9123f35bbc10ddd795b4f8e78a0b28f3cb17
bibushik <bibushik@yandex.ru>
2018-04-14 16:01:01 +0300

    first_tests_messages_reducer

---
+++ src/reducers/messages.spec.js
@@ -0,0 +1,46 @@
+import { expect } from 'chai';
+import messages from './messages';
+import * as ActionType from '../actions/messages';
+
+describe('Reducer::Messages', () => {
+    it('returns items (empty array), next (true), loading(true) if state is undefined', () => {
+        // setup
+        const state = null;
+        const expectedNewState = {
+            items: [],
+            next: true,
+            loading: true,
+        };
+
+        // execute
+        const newState = messages(state, { type: 'unknown' });
+
+        // verify
+        expect(newState).to.deep.equal(expectedNewState);
+    });
+    it('on MESSAGE_ADD returns new state with added messages', () => {
+        // setup
+        const state =
+            {
+                items: [{ name: '1message' }, { name: '2message' }, { name: '3message' }],
+                next: null,
+                loading: false,
+                count: 0,
+            };
+        const newMessage = { name: '4message' };
+        const action = ActionType.addMessage(newMessage);
+        const expectedNewState =
+             {
+                 items: [{ name: '1message' }, { name: '2message' }, { name: '3message' }, { name: '4message' }],
+                 next: null,
+                 loading: false,
+                 count: 0,
+             };
+        // execute
+
+        const newState = messages(state, action);
+
+        // verify
+        expect(newState).to.deep.equal(expectedNewState);
+    });
+});

dc5e58c31b114e9be05b6ee60a728674d1e79da7
bibushik <bibushik@yandex.ru>
2018-04-14 18:11:15 +0300

    add tests to messages reducer

--- src/reducers/messages.js
+++ src/reducers/messages.js
@@ -23,41 +23,44 @@ const messages = (state, action) => {
         return false;
 
     case 'MESSAGE_SEND_ERROR':
-        return action.error;
+        return {
+            ...state,
+            error: action.error
+        };
 
     case 'MESSAGE_SENDING':
         return state;
 
-        case 'MESSAGES_LOADING':
-            return {
-                ...state,
-                loading: action.loading
-            };
-        case 'MESSAGES_LOADED':
-            return {
-                ...state,
-                ...action.messages,
-                items: action.messages.items.reverse().slice(0).concat(state.items)
-            };
-        case 'MESSAGES_RELOAD':
-            return {
-                ...state,
-                next:true,
-                items:[]
-            };
-        case 'MESSAGE_LOAD_ERROR':
-            return {
-                ...state,
-                error: action.error
-            };
-            case 'USER_SIGN_OUT':
-            return {
-                items: DEFAULT_MESSAGES,
-                next:true,
-                loading: true
-            };
-        default:
-            return state;
+    case 'MESSAGES_LOADING':
+        return {
+            ...state,
+            loading: action.loading
+        };
+    case 'MESSAGES_LOADED':
+        return {
+            ...state,
+            ...action.messages,
+            items: action.messages.items.reverse().slice(0).concat(state.items)
+        };
+    case 'MESSAGES_RELOAD':
+        return {
+            ...state,
+            next:true,
+            items:[]
+        };
+    case 'MESSAGE_LOAD_ERROR':
+        return {
+            ...state,
+            error: action.error
+        };
+    case 'USER_SIGN_OUT':
+        return {
+            items: DEFAULT_MESSAGES,
+            next:true,
+            loading: true
+        };
+    default:
+        return state;
     }
 };
 

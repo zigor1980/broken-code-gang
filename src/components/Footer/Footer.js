f767ba2211153b921c51ebf0cf635fb9598501b5
zigor1980 <zihor1980@gmail.com>
2018-04-27 23:58:14 +0300

    Footer.js:Delete unusable vars

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import sendMessage from '../../actions/sendMessage';
-import { Button } from '../Button/Button';
 import './Footer.css';
 
 const stateToProps = state => ({

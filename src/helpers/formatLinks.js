7b376e57fc77a9c52fec8fba013b0c21f30da431
bibushik <bibushik@yandex.ru>
2018-04-28 15:49:20 +0300

    remove is-image package

--- src/helpers/formatLinks.js
+++ src/helpers/formatLinks.js
@@ -1,11 +1,10 @@
 //const regFindUrls = /((http|https)\:\/\/)?[a-zA-Zа-яА-я0-9\-\_\.]+\.[a-zA-Zа-яА-я]{2,3}(\/\S*)?/gmi;
-//const regIsImg = /\.(gif|jpg|jpeg|tiff|png)$/gi;
+const regIsImg = /\.(gif|jpg|jpeg|tiff|png)$/gi;
 const isUrl = require('is-url');
-const isImage = require('is-image');
 
 export default function createFormattedText(text){
     if(isUrl(text)){
-        if(isImage(text)){
+        if(regIsImg.test(text)){
             return {
                 type: 'image',
                 src: text,
